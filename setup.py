7e4fbf22ae42f542525e797090b0e2b8e9d2d7e7
Martin Israel <martin.israel@dlr.de>
2019-01-16 11:15:38 +0100

    setup.py-bugfix pyqtgraph

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(name='poitagger',
         'Pillow'
       ],
       dependency_links = [
-        'https://github.com/pyqtgraph/pyqtgraph/archive/develop.zip'
+        'https://github.com/pyqtgraph/pyqtgraph/archive/develop#egg=pyqtgraph-0.11.0.dev0'
       ],
       package_data={
             '': ['*.js','*.html','*.css','*.ui','ui/*.*','ui/icons/*.*','*.ini'],
