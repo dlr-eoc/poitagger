bf399a3628efb5e0a2c2f9137f1a40a89735c9ae
Israel <Martin.Israel@dlr.de>
2020-04-20 16:17:44 +0200

    calibfiles added

--- poitagger/__init__.py
+++ poitagger/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 import os
-from shutil import copyfile
+from shutil import copyfile,copytree
 
 __version__ = "0.2.8"
 
@@ -12,9 +12,17 @@ PATHS["USER"] = os.path.expanduser("~/poitagger/")
 if not os.path.exists(PATHS["USER"]):
     os.mkdir(PATHS["USER"])
 PATHS["USER_CALIB"] = os.path.join(PATHS["USER"], "calib")
+
+PATHS["BASE"] = pkg_resources.resource_filename('poitagger', '')
     
 if not os.path.exists(PATHS["USER_CALIB"]):
     os.mkdir(PATHS["USER_CALIB"])
+if not os.path.exists(os.path.join(PATHS["USER_CALIB"],"Flir_Boson_320_6,3mm.json")):
+    copyfile(os.path.join(PATHS["BASE"],"calib","Flir_Boson_320_6,3mm.json"),os.path.join(PATHS["USER_CALIB"],"Flir_Boson_320_6,3mm.json"))
+if not os.path.exists(os.path.join(PATHS["USER_CALIB"],"Flir_Boson_640_14mm.json")):
+    copyfile(os.path.join(PATHS["BASE"],"calib","Flir_Boson_640_14mm.json"),os.path.join(PATHS["USER_CALIB"],"Flir_Boson_640_14mm.json"))
+if not os.path.exists(os.path.join(PATHS["USER_CALIB"],"Mavic2_Enterprise_Dual.json")):
+    copyfile(os.path.join(PATHS["BASE"],"calib","Mavic2_Enterprise_Dual.json"),os.path.join(PATHS["USER_CALIB"],"Mavic2_Enterprise_Dual.json"))
 
 PATHS["CONF"] = os.path.join(PATHS["USER"], "poitagger.ini")
 PATHS["POIS"] = os.path.join(PATHS["USER"], "pois.gpx")
@@ -22,7 +30,6 @@ PATHS["POIS"] = os.path.join(PATHS["USER"], "pois.gpx")
 if not os.path.exists(PATHS["CONF"]):
     copyfile(pkg_resources.resource_filename('poitagger', 'conf.ini'), PATHS["CONF"])
 print(PATHS["CONF"])
-PATHS["BASE"] = pkg_resources.resource_filename('poitagger', '')
 PATHS["UI"] = pkg_resources.resource_filename('poitagger', 'ui/')
 PATHS["PROPERTIES"] = pkg_resources.resource_filename('poitagger', 'properties/')
 PATHS["ICONS"] = pkg_resources.resource_filename('poitagger', 'ui/icons/')
